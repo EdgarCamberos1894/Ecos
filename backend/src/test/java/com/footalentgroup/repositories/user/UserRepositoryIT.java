9e85ef88ed5101292d6b2b78dc189cccfb40d615
Zair Bulos <buloszair@gmail.com>
2025-04-29 12:26:52 -0300

    test(backend): Add integration tests and seeders for UserRepository. #14

---
+++ backend/src/test/java/com/footalentgroup/repositories/user/UserRepositoryIT.java
@@ -0,0 +1,41 @@
+package com.footalentgroup.repositories.user;
+
+import com.footalentgroup.TestConfig;
+import com.footalentgroup.models.entities.UserEntity;
+import com.footalentgroup.models.enums.Role;
+import com.footalentgroup.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestConfig
+class UserRepositoryIT {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testFindAll() {
+        List<UserEntity> users = userRepository.findAll();
+        assertEquals(2, users.size());
+    }
+
+    @Test
+    void testFindById() {
+        assertTrue(userRepository.findById(1L).isPresent());
+        UserEntity user1 = userRepository.findById(1L).get();
+        assertEquals("John Doe", user1.getName());
+        assertEquals("john.doe@example.com", user1.getEmail());
+        assertEquals(Role.FAN, user1.getRole());
+
+        assertTrue(userRepository.findById(2L).isPresent());
+        UserEntity user2 = userRepository.findById(2L).get();
+        assertEquals("Jane Doe", user2.getName());
+        assertEquals("jane.doe@example.com", user2.getEmail());
+        assertEquals(Role.MUSICIAN, user2.getRole());
+    }
+}
