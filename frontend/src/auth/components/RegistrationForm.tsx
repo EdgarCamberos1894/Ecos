18a4470aab436b9339daddb89b78dd829a655904
Cecilia Suarez <cecisuarez08@gmail.com>
2025-04-29 02:07:15 -0300

    feat(auth): agregar validación con zod en el formulario de registro #4

--- frontend/src/auth/components/RegistrationForm.tsx
+++ frontend/src/auth/components/RegistrationForm.tsx
@@ -1,15 +1,56 @@
 import Button from "@/shared/Button";
 import Input from "@/shared/Input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const registerSchema = z.object({
+  name: z.string().min(3, { message: "Su nombre es obligatorio" }),
+  email: z.string().email({ message: "El email ingresado no es válido" }),
+  password: z.string().min(8, { message: "Su contraseña debe tener al menos 8 caracteres" }),
+});
+
+export type FormFields = z.infer<typeof registerSchema>;
 
 const RegistrationForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = (formData: FormFields) => {
+    console.log("Datos validados --> ", formData);
+  };
+
   return (
-    <form className="w-full">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        void handleSubmit(onSubmit)(e);
+      }}
+      className="w-full"
+    >
       <div className="mb-9 flex flex-col gap-4">
-        <Input type="text" placeholder="Nombre" />
-        <Input type="email" placeholder="e-mail@mail.com" />
-        <Input type="password" placeholder="Contraseña" />
+        <div>
+          <Input type="text" placeholder="Nombre" {...register("name")} />{" "}
+          {errors.name && <p className="mt-1 h-6 text-red-500">{errors.name.message}</p>}
+        </div>
+        <div>
+          <Input type="email" placeholder="e-mail@mail.com" {...register("email")} />{" "}
+          {errors.email && <p className="mt-1 h-6 text-red-500">{errors.email.message}</p>}
+        </div>
+        <div>
+          <Input type="password" placeholder="Contraseña" {...register("password")} />{" "}
+          {errors.password && <p className="mt-1 h-6 text-red-500">{errors.password.message}</p>}
+        </div>
       </div>
-      <Button type="submit">Registrarse</Button>
+
+      <Button type="submit" disabled={isSubmitting}>
+        {isSubmitting ? "Registrándose..." : "Registrarse"}
+      </Button>
     </form>
   );
 };
