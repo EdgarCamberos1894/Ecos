5d046052a38c53f4730e02260fab656a92e55036
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-29 12:14:51 -0300

    chore: add Header component to MainLayout

--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -1,3 +1,14 @@
-export const Header = () => {
-  return <header>Header</header>;
+interface HeaderProps {
+  onOpenPopup: () => void;
+}
+
+export const Header = ({ onOpenPopup }: HeaderProps) => {
+  return (
+    <header className="flex w-full items-center justify-around bg-[#B1B1B1] p-4 shadow">
+      <button type="button">Iniciar Sesión</button>
+      <button type="button" onClick={onOpenPopup}>
+        Crear cuenta
+      </button>
+    </header>
+  );
 };
--- frontend/src/shared/components/layouts/MainLayout.tsx
+++ frontend/src/shared/components/layouts/MainLayout.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
 import { Header } from "../Header.tsx";
 import { Footer } from "../Footer.tsx";
+import PopUp from "@/shared/components/PopUp.tsx";
+import RoleSelector from "@/auth/components/RoleSelector.tsx";
 
 export const MainLayout = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   return (
     <div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
-      <Header />
+      <Header
+        onOpenPopup={() => {
+          setIsPopupOpen(true);
+        }}
+      />
+      <PopUp
+        isOpen={isPopupOpen}
+        onClose={() => {
+          setIsPopupOpen(false);
+        }}
+        normalText="Registrate en "
+        highlightedText=" ECOS"
+      >
+        <RoleSelector />
+      </PopUp>
       <Outlet />
       <Footer />
     </div>
