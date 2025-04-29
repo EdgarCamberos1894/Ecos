e69d6aec71eeb3584c10f54d4d5a25f353c23331
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-29 10:08:22 -0300

    feat: add basic layout

---
+++ frontend/src/shared/components/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import { Header } from "../Header.tsx";
+import { Footer } from "../Footer.tsx";
+
+export const MainLayout = () => {
+  return (
+    <div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
