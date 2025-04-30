d19fdd254fb8d3284f1607cf7c3038f2ec113b94
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-30 12:34:59 -0300

    chore: adjust RegistrationForm to match Figma design. #3

--- frontend/src/auth/components/RegistrationForm.tsx
+++ frontend/src/auth/components/RegistrationForm.tsx
@@ -1,18 +1,26 @@
 import Button from "@/shared/components/Button";
 import Input from "@/shared/components/Input";
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { EyeOff } from "./ui/EyeOff";
 
 const registerSchema = z.object({
   name: z.string().min(3, { message: "Su nombre es obligatorio" }),
   email: z.string().email({ message: "El email ingresado no es válido" }),
   password: z.string().min(8, { message: "Su contraseña debe tener al menos 8 caracteres" }),
+  terms: z.literal(true, {
+    errorMap: () => ({ message: "Debes aceptar los términos" }),
+  }),
 });
 
 export type FormFields = z.infer<typeof registerSchema>;
 
-const RegistrationForm = ({ role }: { role: string }) => {
+interface RegistrationFormProps {
+  role: string;
+}
+
+const RegistrationForm = ({ role }: RegistrationFormProps) => {
   const {
     register,
     handleSubmit,
@@ -21,35 +29,38 @@ const RegistrationForm = ({ role }: { role: string }) => {
     resolver: zodResolver(registerSchema),
   });
 
-  const onSubmit = (formData: FormFields) => {
-    console.log("Datos validados --> ", { ...formData, role });
+  const onSubmit: SubmitHandler<FormFields> = (formData: FormFields) => {
+    console.log("Datos validados --> ", { ...formData, role }); //TODO --> Eliminar el campo terms para enviar al backend
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        void handleSubmit(onSubmit)(e);
-      }}
-      className="w-full"
-    >
-      <div className="mb-9 flex flex-col gap-4">
-        <div>
-          <Input type="text" placeholder="Nombre" {...register("name")} />{" "}
-          {errors.name && <p className="mt-1 h-6 text-red-500">{errors.name.message}</p>}
-        </div>
-        <div>
-          <Input type="email" placeholder="e-mail@mail.com" {...register("email")} />{" "}
-          {errors.email && <p className="mt-1 h-6 text-red-500">{errors.email.message}</p>}
-        </div>
-        <div>
-          <Input type="password" placeholder="Contraseña" {...register("password")} />{" "}
-          {errors.password && <p className="mt-1 h-6 text-red-500">{errors.password.message}</p>}
-        </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <div>
+        <Input type="text" placeholder="Nombre" {...register("name")} />{" "}
+        {errors.name && <p className="mt-1 h-6 text-red-500">{errors.name.message}</p>}
       </div>
 
-      <Button type="submit" disabled={isSubmitting}>
-        {isSubmitting ? "Registrándose..." : "Registrarse"}
+      <div>
+        <Input type="email" placeholder="e-mail@mail.com" {...register("email")} />{" "}
+        {errors.email && <p className="mt-1 h-6 text-red-500">{errors.email.message}</p>}
+      </div>
+
+      <div className="relative flex items-center">
+        <Input type="password" placeholder="Contraseña" {...register("password")} />{" "}
+        <EyeOff className="absolute right-6" />
+        {errors.password && <p className="mt-1 h-6 text-red-500">{errors.password.message}</p>}
+      </div>
+
+      <label className="flex items-center justify-center gap-2 text-sm">
+        <input type="checkbox" {...register("terms")} className="size-6" />
+        <span className="checkbox-label text-[#6E6E6E]">
+          Leí y acepto los <u className="hover:cursor-pointer">Términos de uso</u>.
+        </span>
+      </label>
+      {errors.terms && <p className="mt-1 h-6 text-red-500">{errors.terms.message}</p>}
+
+      <Button className="hover:cursor-pointer" type="submit" disabled={isSubmitting}>
+        {isSubmitting ? "Registrándose..." : "Registrate"}
       </Button>
     </form>
   );
--- frontend/src/auth/components/RoleSelector.tsx
+++ frontend/src/auth/components/RoleSelector.tsx
@@ -3,7 +3,11 @@ import Button from "@/shared/components/Button";
 import { useState } from "react";
 import RegistrationForm from "./RegistrationForm";
 
-const RoleSelector = () => {
+interface RoleSelectorProps {
+  onChange: () => void;
+}
+
+const RoleSelector = ({ onChange }: RoleSelectorProps) => {
   const [showRegistrationForm, setShowRegistrationForm] = useState(false);
   const [role, setRole] = useState("");
 
@@ -13,10 +17,10 @@ const RoleSelector = () => {
   };
 
   return (
-    <div className="flex h-full flex-col items-center justify-around">
+    <>
       {!showRegistrationForm ? (
-        <>
-          <div className="flex w-full items-center justify-around gap-10">
+        <div className="flex h-full flex-col items-center justify-around p-12">
+          <div className="flex w-full items-center justify-around gap-12">
             <img src={image} alt="image" className="h-[120px] rounded-[16px]" />
             <div className="flex flex-col items-start justify-between gap-4">
               <Button
@@ -53,11 +57,19 @@ const RoleSelector = () => {
               </p>
             </div>
           </div>
-        </>
+        </div>
       ) : (
-        <RegistrationForm role={role} />
+        <div className="flex h-full flex-col items-center justify-center">
+          <RegistrationForm role={role} />
+          <p className="mt-4 text-center text-sm">
+            ¿Ya tienes cuenta?{" "}
+            <u onClick={onChange} className="hover:cursor-pointer">
+              Inicia sesión
+            </u>
+          </p>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
