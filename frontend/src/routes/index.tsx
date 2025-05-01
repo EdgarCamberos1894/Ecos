1afbfc36085395484c3d4ece8a1a4379c6f484ff
Cristian Daniel Toia <toiacristiandaniel@gmail.com>
2025-05-01 17:21:21 -0300

    solucionando conflictos y guargando cambios para nuevo merge

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router";
 import { HomePage } from "@/home/components/HomePage";
 import { MainLayout } from "@/shared/components/layouts/MainLayout";
 import ProfilePage from "@/profiles/components/ProfilePage";
+import ExplorerPage from "@/explorer/components/ExplorerPage";
+import ArtistPage from "@/artist/components/ArtistPage";
+import PlayPage from "@/play/components/PlayPage";
 
 export const AppRoutes = () => {
   return (
@@ -9,6 +12,9 @@ export const AppRoutes = () => {
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<div>404 NOT FOUND</div>} />
+        <Route path="/explorer" element={<ExplorerPage />} />
+        <Route path="/artist" element={<ArtistPage />} />
+        <Route path="/play" element={<PlayPage />} />
       </Route>
       <Route path="/profile" element={<ProfilePage />} />
     </Routes>
