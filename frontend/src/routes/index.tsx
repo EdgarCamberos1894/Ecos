6d6a35d2e0f67e6c9a85f31f2e28e3476cafb06a
Cecilia Suarez <cecisuarez08@gmail.com>
2025-05-01 09:09:25 -0300

    feat: agregar componentes reutilizables y rutas iniciales

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router";
 import { HomePage } from "@/home/components/HomePage";
 import { MainLayout } from "@/shared/components/layouts/MainLayout";
+import ExplorerPage from "@/explorer/components/ExplorerPage";
+import ArtistPage from "@/artist/components/ArtistPage";
+import PlayPage from "@/play/components/PlayPage";
 
 export const AppRoutes = () => {
   return (
@@ -8,6 +11,9 @@ export const AppRoutes = () => {
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<div>404 NOT FOUND</div>} />
+        <Route path="/explorer" element={<ExplorerPage />} />
+        <Route path="/artist" element={<ArtistPage />} />
+        <Route path="/play" element={<PlayPage />} />
       </Route>
     </Routes>
   );
