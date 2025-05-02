06c8d7595b485cfc4a290cdef70ef7e46289e894
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-02 17:40:01 -0300

    refactor: move role logic from Header to AuthModal component

--- frontend/src/shared/components/Header/components/Header.tsx
+++ frontend/src/shared/components/Header/components/Header.tsx
@@ -1,44 +1,25 @@
 import { useState } from "react";
 import { Link } from "react-router";
-import Modal from "../../Modal";
-import LoginForm from "@/auth/components/LoginForm";
-import RoleSelector from "@/auth/components/RoleSelector";
 import { Avatar } from "@/auth/components/ui/Avatar";
 import Input from "../../Input";
+import AuthModal, { AuthMode } from "@/auth/components/AuthModal";
 import MenuIcon from "@/assets/hamburgerMenu.svg?react";
 import Lens from "@/assets/lens.svg?react";
-import ForgotPasswordForm from "@/auth/components/ForgotPasswordForm";
 import { useAuth } from "@/auth/hooks/use-auth";
 import UserMenu from "@/auth/components/UserMenu";
 import { Bell } from "../ui/Bell";
 
 export const Header = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalMode, setModalMode] = useState<"login" | "register" | "forgot">();
+  const [openModal, setOpenModal] = useState<AuthMode | null>(null);
 
   const { user } = useAuth();
 
-  const handleOpenLogin = () => {
-    setModalMode("login");
-    setIsModalOpen(true);
-  };
-
-  const handleOpenRegister = () => {
-    setModalMode("register");
-    setIsModalOpen(true);
+  const handleOpenModal = (mode: AuthMode) => {
+    setOpenModal(mode);
   };
 
   const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleChangeModal = () => {
-    if (modalMode === "login") {
-      setModalMode("register");
-      return;
-    }
-
-    setModalMode("login");
+    setOpenModal(null);
   };
 
   return (
@@ -61,10 +42,20 @@ export const Header = () => {
         {!user ? (
           <>
             <div className="inline-flex shrink items-center justify-center gap-6">
-              <button type="button" onClick={handleOpenLogin}>
+              <button
+                type="button"
+                onClick={() => {
+                  handleOpenModal("login");
+                }}
+              >
                 Iniciar Sesión
               </button>
-              <button type="button" onClick={handleOpenRegister}>
+              <button
+                type="button"
+                onClick={() => {
+                  handleOpenModal("register");
+                }}
+              >
                 Crear cuenta
               </button>
             </div>
@@ -77,47 +68,8 @@ export const Header = () => {
           </>
         )}
       </header>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        normalText={
-          modalMode === "login"
-            ? "Inicia sesión en "
-            : modalMode === "register"
-              ? "Registrate en "
-              : "Olvidé mi contraseña"
-        }
-        highlightedText={modalMode !== "forgot" ? "ECOS" : ""}
-      >
-        {modalMode === "login" && (
-          <div className="flex h-full flex-col items-center justify-center">
-            <LoginForm />
-            <div className="mt-9 flex flex-col items-center text-sm text-[#6E6E6E]">
-              <u
-                onClick={() => {
-                  setModalMode("forgot");
-                }}
-                className="hover:cursor-pointer"
-              >
-                ¿Olvidaste tu contraseña?
-              </u>
-              <p>
-                ¿No tienes una cuenta?{" "}
-                <u onClick={handleChangeModal} className="hover:cursor-pointer">
-                  Regístrate
-                </u>
-              </p>
-            </div>
-          </div>
-        )}
-        {modalMode === "register" && <RoleSelector onChange={handleChangeModal} />}
 
-        {modalMode === "forgot" && (
-          <div className="flex h-full flex-col items-center justify-center">
-            <ForgotPasswordForm onChange={handleChangeModal} />
-          </div>
-        )}
-      </Modal>
+      {openModal && <AuthModal mode={openModal} onClose={handleCloseModal} />}
     </>
   );
 };
