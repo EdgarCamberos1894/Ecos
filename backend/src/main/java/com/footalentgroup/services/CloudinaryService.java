982c4b4a40bd41f9bce787dba515167ddf71d2c7
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2025-05-02 04:52:39 -0700

    chore(backend): remove unused imports and update cloudinary dependency to avoid errors

--- backend/src/main/java/com/footalentgroup/services/CloudinaryService.java
+++ backend/src/main/java/com/footalentgroup/services/CloudinaryService.java
@@ -1,13 +1,7 @@
 package com.footalentgroup.services;
 
-import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.Map;
 
 public interface CloudinaryService {
