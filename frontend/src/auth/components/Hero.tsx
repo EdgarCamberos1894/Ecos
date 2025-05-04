46d6abb58180e22dd77db8c049a75c81b61c2a96
Cristian Daniel Toia <toiacristiandaniel@gmail.com>
2025-05-04 14:14:06 -0300

    creación de componentes OptionRegisterCards.tsx, OptionRegister.tsx y Hero.tsx implementados en el componente ya existente HomePage.tsx para su renderizado

---
+++ frontend/src/auth/components/Hero.tsx
@@ -0,0 +1,30 @@
+export default function Hero() {
+  return (
+    <div className="flex h-216 max-w-[1920px] flex-col justify-start bg-[url('@/assets/imageBanner.webp')] bg-cover bg-center">
+      <div className="ms-48 w-120">
+        <h1 className="mt-38 text-start font-sans text-[42px] leading-14 font-bold">
+          Bienvenidos a Ecos
+          <br />
+          tu plataforma
+          <br />
+          musical ideal
+        </h1>
+        <p className="mt-4 text-start text-[14px]">
+          Descubre un mundo lleno de música y creatividad. Únete a
+          <br />
+          nosotros para compartir tu arte y conectar con otros amantes
+          <br />
+          de la música
+        </p>
+        <div className="mt-6 flex gap-12">
+          <button type="submit" className="h-10 w-32 rounded-3xl bg-[#707070] text-sm text-white">
+            Registrate
+          </button>
+          <button type="submit" className="h-10 w-32 rounded-3xl bg-[#B1B1B1] text-sm text-white">
+            Explora
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/home/components/HomePage.tsx
+++ frontend/src/home/components/HomePage.tsx
@@ -1,3 +1,11 @@
+import Hero from "@/auth/components/Hero";
+import OptionsRegister from "@/auth/components/OptionsRegister";
+
 export const HomePage = () => {
-  return <main className="content-center">HomePage</main>;
+  return (
+    <main className="content-center">
+      <Hero />
+      <OptionsRegister />
+    </main>
+  );
 };
