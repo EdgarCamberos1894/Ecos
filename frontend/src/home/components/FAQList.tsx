5030ed227fc40e83b0a82f229abca03e342921a2
GinoGallardo <ginosgt@gmail.com>
2025-05-05 20:25:36 -0300

    Update stilos de preguntas frecuentes

--- frontend/src/home/components/FAQList.tsx
+++ frontend/src/home/components/FAQList.tsx
@@ -2,7 +2,7 @@ import FAQItem from "./FAQItem";
 
 const FAQList = () => {
   return (
-    <section className="justyfi-center ms-52 mt-24 flex max-w-[1920px] flex-col items-center">
+    <section className="my-24 flex flex-col items-center">
       <h2 className="mb-8 text-start font-[roboto] text-[40px]">Preguntas frecuentes</h2>
       <FAQItem />
     </section>
