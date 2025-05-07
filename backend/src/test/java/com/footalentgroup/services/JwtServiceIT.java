e853bef3c67d7a443fdaf82ae150d03199630fa6
Zair Bulos <buloszair@gmail.com>
2025-05-07 11:31:12 -0300

    refactor(backend): Improve JWT authentication flow and refresh token management

--- backend/src/test/java/com/footalentgroup/services/JwtServiceIT.java
+++ backend/src/test/java/com/footalentgroup/services/JwtServiceIT.java
@@ -1,10 +1,13 @@
 package com.footalentgroup.services;
 
 import com.footalentgroup.TestConfig;
+import com.footalentgroup.models.entities.UserEntity;
+import com.footalentgroup.models.enums.Role;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
 class JwtServiceIT {
@@ -12,19 +15,59 @@ class JwtServiceIT {
     @Autowired
     private JwtService jwtService;
 
+    private UserEntity user;
+
+    @BeforeEach
+    void setup() {
+        user = UserEntity.builder()
+                .id(1L)
+                .name("name")
+                .email("email@example.com")
+                .role(Role.FAN)
+                .build();
+    }
+
     @Test
     void testExtractToken() {
         assertEquals("t.t.t", jwtService.extractToken("Bearer t.t.t"));
     }
 
     @Test
-    void testCreateAndVerifyToken() {
-        String token = jwtService.createToken("email@example.com", "name", 1L,"ROLE");
+    void testGenerateAndVerifyAccessToken() {
+        String accessToken = jwtService.generateAccessToken(user);
 
-        assertEquals(3, token.split("\\.").length);
-        assertEquals("email@example.com", jwtService.email(token));
-        assertEquals("name", jwtService.name(token));
-        assertEquals(1L, jwtService.id(token));
-        assertEquals("ROLE", jwtService.role(token));
+        assertNotNull(accessToken);
+        assertEquals(3, accessToken.split("\\.").length);
+
+        assertEquals(1L, jwtService.id(accessToken));
+        assertEquals("name", jwtService.name(accessToken));
+        assertEquals("email@example.com", jwtService.email(accessToken));
+        assertEquals("FAN", jwtService.role(accessToken));
+
+        assertFalse(jwtService.isTokenExpired(accessToken));
+    }
+
+    @Test
+    void testGenerateAndVerifyRefreshToken() {
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        assertNotNull(refreshToken);
+        assertEquals(3, refreshToken.split("\\.").length);
+
+        assertEquals(1L, jwtService.id(refreshToken));
+        assertEquals("name", jwtService.name(refreshToken));
+        assertEquals("email@example.com", jwtService.email(refreshToken));
+        assertEquals("FAN", jwtService.role(refreshToken));
+
+        assertFalse(jwtService.isTokenExpired(refreshToken));
+    }
+
+    @Test
+    void testExpiredAccessToken() throws InterruptedException {
+        String accessToken = jwtService.generateAccessToken(user);
+
+        Thread.sleep((5 + 1) * 1000);
+
+        assertTrue(jwtService.isTokenExpired(accessToken));
     }
 }
