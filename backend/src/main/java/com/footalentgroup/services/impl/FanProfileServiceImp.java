bd182d18f77e8bdf79c5687729dfbef3e922bab2
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-05-10 18:02:08 -0300

    feat(backend): Add createFanProfile method, in FanProfileServiceImp, to handle fan profile creation

--- backend/src/main/java/com/footalentgroup/services/impl/FanProfileServiceImp.java
+++ backend/src/main/java/com/footalentgroup/services/impl/FanProfileServiceImp.java
@@ -2,10 +2,18 @@ package com.footalentgroup.services.impl;
 
 import com.footalentgroup.models.dtos.request.FanProfileRequestDto;
 import com.footalentgroup.models.dtos.response.FanProfileResponseDto;
+import com.footalentgroup.models.entities.FanProfileEntity;
 import com.footalentgroup.models.entities.UserEntity;
+import com.footalentgroup.repositories.FanProfileRepository;
 import com.footalentgroup.services.FanProfileService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public class FanProfileServiceImp  implements FanProfileService {
+@AllArgsConstructor
+@Service
+public class FanProfileServiceImp implements FanProfileService {
+    private final FanProfileRepository fanProfileRepository;
+
     @Override
     public FanProfileResponseDto getFanProfileById(Long id) {
         return null;
@@ -13,7 +21,9 @@ public class FanProfileServiceImp  implements FanProfileService {
 
     @Override
     public void createFanProfile(UserEntity user) {
-
+        FanProfileEntity fan = new FanProfileEntity();
+        fan.setUser(user);
+        fanProfileRepository.save(fan);
     }
 
     @Override
