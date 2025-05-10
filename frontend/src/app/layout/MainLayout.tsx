1dc30a1f9a6e3607b4b7fcc8f10a98f11098d6ee
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-10 13:59:07 -0300

    Update MainLayout.tsx

--- frontend/src/app/layout/MainLayout.tsx
+++ frontend/src/app/layout/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Footer } from "./Footer/Footer.tsx";
 
 export const MainLayout = () => {
   return (
-    <div className="min-h-dvh grid-rows-[auto_1fr_auto]">
+    <div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
       <Header />
       <Outlet />
       <Footer />
