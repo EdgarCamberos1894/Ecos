29565b4e39cd49df9ec6e9ca8b3e99aba090b796
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-05-11 12:49:38 -0300

    feat(backend): add MusicianFollowsEntity and set up relations with Musician and Fan entities

---
+++ backend/src/main/java/com/footalentgroup/models/entities/MusicianFollowsEntity.java
@@ -0,0 +1,33 @@
+package com.footalentgroup.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "musician_follows", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"musician_id", "fan_id"})
+})
+public class MusicianFollowsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @CreationTimestamp
+    @Column(updatable = false)
+    private OffsetDateTime created_at;
+
+    @ManyToOne
+    @JoinColumn(name = "musician_id", nullable = false)
+    private MusicianProfileEntity musician;
+
+    @ManyToOne
+    @JoinColumn(name = "fan_id", nullable = false)
+    private FanProfileEntity fan;
+}
