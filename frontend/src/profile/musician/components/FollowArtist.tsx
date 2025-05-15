7dc4a9b5afa1b729f9d15b013b57ded0ba820ab1
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-15 00:25:14 -0300

    feat: add social media links for the musician

--- frontend/src/profile/musician/components/FollowArtist.tsx
+++ frontend/src/profile/musician/components/FollowArtist.tsx
@@ -1,15 +1,27 @@
 import { SpotifyIcon } from "@/app/ui/SpotifyIcon";
 import { YoutubeIcon } from "@/app/ui/YoutubeIcon";
 import { InstagramIcon } from "@/app/ui/InstagramIcon";
+import { Link, useParams } from "react-router";
+import { MusicianProfile } from "../musician-types";
+import { useApiQuery } from "@/shared/hooks/use-api-query";
 
 const FollowArtist = () => {
+  const { id } = useParams() as { id: string };
+  const { data: profile } = useApiQuery<MusicianProfile>("profile", `musician-profile/${id}`, id);
+
   return (
     <div className="space-y-6">
       <h2 className="text-ecos-blue text-2xl font-medium uppercase">Seguinos</h2>
       <div className="flex items-center gap-9">
-        <SpotifyIcon className="size-13 cursor-pointer" />
-        <YoutubeIcon className="size-14 cursor-pointer" />
-        <InstagramIcon className="size-12 cursor-pointer" />
+        <Link title="Spotify" to={profile?.data.spotifyUrl ?? "#"}>
+          <SpotifyIcon className="size-13 cursor-pointer" />
+        </Link>
+        <Link title="Youtube" to={profile?.data.youtubeUrl ?? "#"}>
+          <YoutubeIcon className="size-14 cursor-pointer" />
+        </Link>
+        <Link title="Instagram" to={profile?.data.instagramUrl ?? "#"}>
+          <InstagramIcon className="size-12 cursor-pointer" />
+        </Link>
       </div>
     </div>
   );
