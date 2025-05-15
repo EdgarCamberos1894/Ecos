87fdbf76700a64f2be8624a454fa12766172613d
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-15 16:20:29 -0300

    feat: add backend endpoint for sending emails

--- frontend/src/profile/musician/components/ContactForm.tsx
+++ frontend/src/profile/musician/components/ContactForm.tsx
@@ -1,29 +1,68 @@
+import { useApiMutation } from "@/shared/hooks/use-api-mutation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const ContactSchema = z.object({
   name: z.string().min(3, { message: "Tu nombre no puede tener menos de 3 caracteres" }),
-  email: z.string().email({ message: "El email ingresado no es válido" }),
+  senderEmail: z.string().email({ message: "El email ingresado no es válido" }),
   message: z.string().max(300, { message: "Tu mensaje como máximo puede tener 300 caracteres" }),
 });
 
 type FormFields = z.infer<typeof ContactSchema>;
 
-const ContactForm = () => {
+interface ContactFormProps {
+  musicianId: number;
+}
+
+interface Body {
+  subject: string;
+  message: string;
+  senderEmail: string;
+  musicianId: number;
+}
+
+interface ContactResponse {
+  message: string;
+}
+
+const ContactForm = ({ musicianId }: ContactFormProps) => {
   const {
     register,
     handleSubmit,
-    // setError,
+    setError,
     formState: { errors },
   } = useForm<FormFields>({
     resolver: zodResolver(ContactSchema),
   });
 
-  const onSubmit: SubmitHandler<FormFields> = (data: FormFields) => {
-    console.log(data);
+  const { mutate, isPending } = useApiMutation<ContactResponse, Body>(
+    "musician-profile/contact",
+    "POST",
+  );
 
-    // TODO: enviar un email al usuario con los datos del formulario y manejar errores con el setError
+  const onSubmit: SubmitHandler<FormFields> = (data: FormFields) => {
+    const { message, name, senderEmail } = data;
+
+    const body = {
+      subject: `Ecos - ${name}`,
+      message,
+      senderEmail,
+      musicianId,
+    };
+
+    mutate(body, {
+      onSuccess: (response) => {
+        toast.success(response.message);
+      },
+      onError: (error) => {
+        toast.error("Error al enviar correo");
+        setError("root", {
+          message: error.message,
+        });
+      },
+    });
   };
 
   return (
@@ -48,14 +87,14 @@ const ContactForm = () => {
       <label className="flex flex-col gap-2 text-2xl font-normal" htmlFor="email">
         Email
         <input
-          id="email"
+          id="senderEmail"
           type="text"
           placeholder="Escribí tu email"
           className="border border-gray-300 p-3 text-sm"
-          {...register("email")}
+          {...register("senderEmail")}
         />
-        {errors.email && (
-          <span className="mt-1 h-6 text-sm text-red-500">{errors.email.message}</span>
+        {errors.senderEmail && (
+          <span className="mt-1 h-6 text-sm text-red-500">{errors.senderEmail.message}</span>
         )}
       </label>
 
@@ -73,8 +112,12 @@ const ContactForm = () => {
         )}
       </label>
 
-      <button type="submit" className="bg-ecos-blue cursor-pointer rounded-full py-2.5 text-white">
-        Enviar
+      <button
+        type="submit"
+        disabled={isPending}
+        className="bg-ecos-blue cursor-pointer rounded-full py-2.5 text-white"
+      >
+        {isPending ? "Enviando" : "Enviar"}
       </button>
 
       {errors.root && <p className="text-red-500">{errors.root.message}</p>}
