f43a6654a44f215da4bcfb6935b92ee2ac0c51a0
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-16 15:58:48 -0300

    chore: fix responsive design

--- frontend/src/profile/musician/EditProfileMusicianPage.tsx
+++ frontend/src/profile/musician/EditProfileMusicianPage.tsx
@@ -154,27 +154,27 @@ export const EditProfileMusicianPage = () => {
 
   return (
     <main className="space-y-32">
-      <section className="relative">
+      <section className="justify-items-center xl:relative">
         <img
           src={banner?.bannerUrl ?? imagePreview ?? ImageBanner}
           alt={`Banner`}
-          className="mb-10 max-h-[680px] w-full object-cover"
+          className="mb-10 hidden h-[clamp(140px,35.4vw,680px)] w-full object-cover xl:block"
         />
         <BannerUploader
           onUpload={handleBannerUpload}
           isUploading={isBannerPending}
           onImageUpload={onImageUpload}
-          className="absolute top-1/2 left-1/2 z-10 flex h-[454px] w-[734px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-7 bg-white/50"
+          className="flex h-[454px] w-full max-w-[734px] flex-col content-center items-center justify-center justify-items-center gap-7 rounded-[20px] p-4 xl:absolute xl:top-1/2 xl:left-1/2 xl:z-10 xl:flex xl:-translate-x-1/2 xl:-translate-y-1/2 xl:bg-white/50"
         />
       </section>
-      <section className="text-ecos-blue mb-24 space-y-2 px-[clamp(16px,8vw,160px)]">
+      <section className="text-ecos-blue mb-24 space-y-2 px-4 sm:px-12 lg:px-[clamp(48px,8vw,160px)]">
         <h2 className="text-4xl">¡Bienvenido!</h2>
-        <h1 className="text-8xl font-bold">{user.name}</h1>
-        <h4 className="mt-6 mb-10 text-2xl font-medium">Editar Panel</h4>
-        <h3 className="text-2xl font-bold uppercase">
+        <h1 className="text-[clamp(2.5rem,10vw,6rem)] font-bold">{user.name}</h1>
+        <h4 className="mt-6 mb-10 text-2xl font-medium text-wrap">Editar Panel</h4>
+        <h3 className="text-2xl font-bold text-balance uppercase">
           Compartí tu música a través de spotify o subí tu archivo mp3/wav
         </h3>
-        <section className="text-ecos-blue mt-12 mb-24 flex w-[777px] flex-col gap-9 rounded-2xl border px-4 py-3">
+        <section className="text-ecos-blue mt-12 mb-24 flex w-full max-w-[clamp(320px,90vw,777px)] flex-col gap-9 rounded-2xl border px-4 py-3">
           <label htmlFor="title" className="flex flex-col gap-3.5 text-2xl">
             Título
             <input
@@ -185,7 +185,7 @@ export const EditProfileMusicianPage = () => {
               onChange={(event) => {
                 setMusicData({ ...musicData, title: event.target.value });
               }}
-              className="border-ecos-dark-grey-light border px-6 py-3.5 text-lg focus:outline-none"
+              className="border-ecos-dark-grey-light rounded-[20px] border px-6 py-3.5 text-lg focus:outline-none"
             />
             <p className="text-sm">El audio puede ser formato mp3 o wav</p>
           </label>
@@ -198,7 +198,7 @@ export const EditProfileMusicianPage = () => {
                 onChange={(event) => {
                   setMusicData({ ...musicData, genre: event.target.value });
                 }}
-                className="border-ecos-dark-grey w-full appearance-none rounded-md border px-3.5 py-3 pr-10 text-lg focus:outline-none"
+                className="border-ecos-dark-grey w-full appearance-none rounded-[20px] border px-3.5 py-3 pr-10 text-lg focus:outline-none"
               >
                 <option value="" disabled>
                   Seleccioná un género
@@ -214,7 +214,7 @@ export const EditProfileMusicianPage = () => {
           </label>
           <label htmlFor="sourceType" className="flex flex-col gap-3.5 text-2xl">
             Elegí como subir tu música
-            <div className="border-ecos-dark-grey-light relative border px-6 py-3.5 text-center">
+            <div className="border-ecos-dark-grey-light relative rounded-[20px] border px-6 py-3.5 text-center">
               <select
                 id="sourceType"
                 value={musicData.sourceType}
@@ -224,12 +224,12 @@ export const EditProfileMusicianPage = () => {
                     sourceType: event.target.value as MusicData["sourceType"],
                   });
                 }}
-                className="border-ecos-dark-grey w-[270px] appearance-none rounded-md border px-3.5 py-3 text-[22px] font-bold focus:outline-none"
+                className="border-ecos-dark-grey mx-auto w-[270px] appearance-none rounded-[20px] border px-3.5 py-3 text-[22px] font-bold focus:outline-none"
               >
                 <option value="SPOTIFY">Spotify</option>
                 <option value="FILE">Mp3/Wav</option>
               </select>
-              <ArrowDown className="text-ecos-dark-grey pointer-events-none absolute top-1/2 right-64 h-5 w-5 -translate-y-1/2" />
+              <ArrowDown className="text-ecos-dark-grey pointer-events-none absolute top-1/2 right-[calc(50%-135px+10px)] h-5 w-5 -translate-y-1/2" />
             </div>
           </label>
           {musicData.sourceType === "SPOTIFY" ? (
@@ -244,12 +244,12 @@ export const EditProfileMusicianPage = () => {
         </section>
         <CreateEventoCard />
       </section>
-      <div className="mr-40 mb-24 flex justify-end gap-12">
+      <div className="mb-24 flex flex-col gap-6 self-center px-4 sm:flex-row sm:justify-start lg:px-[clamp(48px,8vw,160px)] xl:justify-end">
         <button
           type="button"
           onClick={handleSubmit}
           disabled={isSongsPending}
-          className="bg-ecos-orange-light cursor-pointer rounded-full px-[120px] py-5 text-base text-white"
+          className="bg-ecos-orange-light cursor-pointer rounded-full py-5 text-base text-white sm:px-[120px]"
         >
           {isSongsPending ? "Guardando" : "Guardar"}
         </button>
