6c5d214bafff9f4662174fabe06033e6335224f6
Cecilia Suarez <cecisuarez08@gmail.com>
2025-05-16 08:14:14 -0300

    style(home): modificación de estilos de las secciones para que se adapten a distintos dispositivos y diseño similar a figma

--- frontend/src/home/components/sections/FAQList.tsx
+++ frontend/src/home/components/sections/FAQList.tsx
@@ -2,8 +2,10 @@ import FAQItem from "../FAQItem";
 
 const FAQList = () => {
   return (
-    <section className="my-24 flex flex-col items-center text-[#19233A]">
-      <h2 className="mb-8 text-start text-5xl lg:mb-36">Preguntas frecuentes</h2>
+    <section className="mb-24 flex w-full flex-col items-center px-2.5 text-[#19233A] lg:p-0">
+      <h2 className="mb-8 w-full text-center text-5xl font-medium lg:mb-36">
+        Preguntas frecuentes
+      </h2>
       <FAQItem />
     </section>
   );
