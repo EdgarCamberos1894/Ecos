b2baeca6661a08976fcef750db6599144770985a
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-22 21:22:20 -0300

    feat: add logic to check hash on URL

--- frontend/src/home/HomePage.tsx
+++ frontend/src/home/HomePage.tsx
@@ -5,8 +5,19 @@ import UpcomingEvents from "./components/sections/UpcomingEvents";
 import FAQList from "./components/sections/FAQList";
 import DiscoverArtist from "./components/sections/DiscoverArtist";
 import BannerGuitarsRegister from "./components/sections/BannerGuitarsRegister";
+import { useLocation } from "react-router";
+import { useEffect } from "react";
 
 export const HomePage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const section = document.getElementById(location.hash.slice(1));
+      if (section) section.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [location]);
+
   return (
     <main className="content-center">
       <Hero />
