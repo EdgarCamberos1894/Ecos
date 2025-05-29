af4b507d5dd5d44228ed3680af4e771cc7e93f50
Cecilia Suarez <cecisuarez08@gmail.com>
2025-05-28 22:14:53 -0300

    refactor(footer): eliminar secciones, actualizar estilos y enlaces según diseño de Figma

--- frontend/src/home/components/sections/DiscoverArtist.tsx
+++ frontend/src/home/components/sections/DiscoverArtist.tsx
@@ -4,7 +4,7 @@ import FeaturedSongs from "../FeaturedSongs";
 const DiscoverArtist = () => {
   return (
     <section
-      id="#explorar"
+      id="explorar"
       className="noScrollbar w-full max-w-screen items-center gap-y-8 overflow-visible overflow-x-auto pr-[0.813rem] md:pr-[4.625rem] lg:pr-8"
     >
       <MusicSearch />
